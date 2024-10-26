ba2cb56ba875d5259034d97435bf4d37fd8d5809
Saumya Puthenveettil <sputhen2000@gmail.com>
2024-10-26 22:57:25 +0000

    Completed Coding Challenge: Array Absolute Value

--- src/main/java/ArrayAbsoluteValue.java
+++ src/main/java/ArrayAbsoluteValue.java
@@ -1,4 +1,4 @@
-
+import java.util.*;
 public class ArrayAbsoluteValue {
     /**
      * Given an array of integers 'nums', produce an array of integers composed of the absolute values of all the
@@ -9,6 +9,11 @@ public class ArrayAbsoluteValue {
      * @return the absolute value array of nums.
      */
     public int[] getArrayAbs(int[] nums){
-        return null;
+
+        for (int i = 0; i<nums.length; i++){
+            nums[i] = Math.abs(nums[i]);
+        }
+        return nums;
+
     }
 }
